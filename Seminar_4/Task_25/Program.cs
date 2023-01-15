bf79158f9f72d4e224d41661ab99df287df57803
Nikita Dushin <overlordnick40@gmail.com>
2023-01-15 20:25:42 +0700

    Сделал задание №27

--- Seminar_4/Task_25/Program.cs
+++ Seminar_4/Task_25/Program.cs
@@ -2,34 +2,35 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-// Console.WriteLine("Введи основание: ");
-// int A = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введи показатель степени: ");
-// int B = Convert.ToInt32(Console.ReadLine());
-// int C = A;
-// for(int i=0; i<B; i++)
-// {
-//     C *= A;    
-// }
-// Console.Write($"{A} в степени {B} равно {C}");
-
-
-int Exponentiation(int base1, int degree) // Возведение числа base1 в степень degree
-{
-int num = base1;
-for(int i=0; i<degree; i++)
-{
-    num *= base1;    
-}
-    return num;
-}
-
 
 Console.WriteLine("Введи основание: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введи показатель степени: ");
 int B = Convert.ToInt32(Console.ReadLine());
+int C = A;
+for (int i = 0; i < B; i++)
+{
+    C *= A;
+}
+Console.Write($"{A} в степени {B} равно {C}");
 
 
-int C = Exponentiation(A, B);
-Console.WriteLine($"{A} в степени {B} равно {C}");
+// // Попробовал через функцию
+// int Exponentiation(int base1, int degree) // Возведение числа base1 в степень degree
+// {
+// int num = base1;
+// for(int i=0; i<degree; i++)
+// {
+//     num *= base1;    
+// }
+//     return num;
+// }
+
+
+// Console.WriteLine("Введи основание: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введи показатель степени: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int C = Exponentiation(A, B);
+// Console.WriteLine($"{A} в степени {B} равно {C}");
--- Seminar_4/Task_27/Program.cs
+++ Seminar_4/Task_27/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Напиши число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (num != 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.Write(sum);
+
+
+// // Решение через массив
+// Console.WriteLine("Напиши положительное целое число: ");
+// string num = Console.ReadLine();
+// int sum = 0;
+// for (int i = 0; i < num.Length; i++)
+// {
+//     sum = sum + int.Parse(Convert.ToString(num[i])); // Не понимаю, как работает конвертация, приходиться гуглить и даже тогда слабо понятно
+// }
+
+// Console.WriteLine(sum);
